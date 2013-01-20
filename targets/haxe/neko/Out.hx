282d62182f9a60a7ac8fad1d3e10af87cf2ffcbc
tangentstorm <michal.wallace@gmail.com>
2013-01-20 00:20:50 -0600

    created a simple Out module for haxe/neko backend

---
+++ targets/haxe/neko/Out.hx
@@ -0,0 +1,12 @@
+
+class Out {
+  public static function String( s:String ) {
+    neko.Lib.print( s );
+  }
+  public static function Int( i:Int ) {
+    neko.Lib.print( i );
+  }
+  public static function Ln( ) {
+    neko.Lib.print( '\n' );
+  }
+}
